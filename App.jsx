728f08b329d6a75bc3439d573bd094bc63ebd5aa
Subhi C <99975184+Subhi-c@users.noreply.github.com>
2023-11-29 21:23:08 +0530

    Add files via upload
    files updated

---
+++ App.jsx
@@ -0,0 +1,55 @@
+import "./index.css";
+import { useState } from "react";
+
+const Messages = ["Learn React", "Apply for Jobs.", "Invest new income"];
+
+function App() {
+  const [step, setStep] = useState(1);
+  let [isOpen, setOpen] = useState(true);
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep((s) => s + 1);
+    console.log(step);
+  }
+
+  return (
+    <div>
+      <div className="close" onClick={() => setOpen((is) => !is)}>
+        &times;
+      </div>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            Step{step} : {Messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
